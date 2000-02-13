362848034f11335b0082f62055fba55d906ada92
Alex Waugh <alex@alexwaugh.com>
2000-02-13 22:46:51 +0000

    Added GNU GPL Added CreateZips script

--- ShapeSort/!RunImage.c
+++ ShapeSort/!RunImage.c
@@ -1,7 +1,21 @@
 /*
 	ShapeSort
-	©Alex Waugh 1998
-	See !Help file for conditions of use
+	© Alex Waugh 1998
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 */
 
 #include "Desk.Window.h"
@@ -338,7 +352,7 @@ void IconBarMenuClick(int item, void *r)
 
 void LoadDrawFiles(void)
 {
-	int offset=0,number=1,x,y,i;
+	int offset=0,number=1,x=1,y=1,i;
 	int currentx,currenty;
 	char buffer[256+LENGTHOFPICTURESDIR+1]=PICTURESDIR".";
 	Desk_Filing_ReadDirNames(PICTURESDIR,buffer+LENGTHOFPICTURESDIR+1,&number,&offset,256,NULL);
